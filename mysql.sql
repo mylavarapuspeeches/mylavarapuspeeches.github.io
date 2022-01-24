f4f3d8bc4a60cc7c895ce86102d38aa4355129ff
mylavarapuspeeches <96851390+mylavarapuspeeches@users.noreply.github.com>
2022-01-24 00:16:11 -0500

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,3 @@
+-- cPanel mysql backup
+GRANT USAGE ON *.* TO 'himamsu_wrdp1'@'localhost' IDENTIFIED BY PASSWORD '*CC972C0FC345D0EAED35A1BAAFE5BAE5EDD81C01';
+GRANT ALL PRIVILEGES ON `himamsu\_wrdp1`.* TO 'himamsu_wrdp1'@'localhost';
